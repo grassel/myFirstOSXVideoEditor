0db5c45a77c993bd00b9ea244f1ffa843c2a8c3a
Guido Grassel <guido@mbp17.grassel.org>
2014-10-05 19:03:38 +0300

    Initial Commit

---
+++ myFirstVideoEditor/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  myFirstVideoEditor
+//
+//  Created by Guido Grassel on 05/10/14.
+//  Copyright (c) 2014 Guido Grassel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
