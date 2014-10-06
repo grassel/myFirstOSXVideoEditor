68014ab9edb42c316da9f7f7c6825948646d4aef
Guido Grassel <guido@mbp17.grassel.org>
2014-10-06 19:49:37 +0300

    generating thumbs from all mp4 files in Movies dir working

--- myFirstVideoEditor/ClipSelectionPanel.swift
+++ myFirstVideoEditor/ClipSelectionPanel.swift
@@ -17,27 +17,26 @@ class ClipSelectionPanel: NSPanel {
         if let moviesDirectory = filemgr.URLForDirectory(NSSearchPathDirectory.MoviesDirectory, inDomain: NSSearchPathDomainMask.UserDomainMask, appropriateForURL: nil, create: false, error: nil) {
         
             println("Looking for clips at \(moviesDirectory.path) ...");
-                
-     //       var clips : NSArray! = filemgr.contentsOfDirectoryAtPath(moviesDirectory.path!, error: nil)
+            
+            //       var clips : NSArray! = filemgr.contentsOfDirectoryAtPath(moviesDirectory.path!, error: nil)
             var properties = NSArray(objects: NSURLLocalizedNameKey, NSURLCreationDateKey, NSURLLocalizedTypeDescriptionKey)
-             var clips : NSArray! = filemgr.contentsOfDirectoryAtURL(moviesDirectory, includingPropertiesForKeys: properties, options: NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants, error: nil)
+            var clips : NSArray! = filemgr.contentsOfDirectoryAtURL(moviesDirectory, includingPropertiesForKeys: properties, options: NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants, error: nil)
             if clips != nil {
                 for index in 0...clips!.count-1 {
-                    var clipPath = clips[index] as NSURL
-                    println("   \(clipPath)");
-
-                    var avasset : AVAsset = AVURLAsset(URL: clipPath, options: nil)
-                    
-                    var generator : AVAssetImageGenerator = AVAssetImageGenerator(asset: avasset);
-                    generator.appliesPreferredTrackTransform = true;
-                    var time : CMTime = CMTimeMake(1,2);
-                    var imageDimen = CGSize(width: 240, height: 160)
-                    generator.maximumSize = imageDimen
-                    var oneRef : CGImageRef = generator.copyCGImageAtTime(time, actualTime: nil, error: nil);
-                    var image : NSImage = NSImage(CGImage: oneRef, size: imageDimen)
+                    var clipUrl = clips[index] as NSURL
+                    if clipUrl.pathExtension == "mp4" {
+                        println("\(clipUrl.path)")
+                        var avasset : AVAsset = AVURLAsset(URL: clipUrl, options: nil)
+                        var generator : AVAssetImageGenerator = AVAssetImageGenerator(asset: avasset);
+                        generator.appliesPreferredTrackTransform = true;
+                        var time : CMTime = CMTimeMake(1,2);
+                        var imageDimen = CGSize(width: 240, height: 160)
+                        generator.maximumSize = imageDimen
+                        var oneRef : CGImageRef = generator.copyCGImageAtTime(time, actualTime: nil, error: nil);
+                        var image : NSImage! = NSImage(CGImage: oneRef, size: imageDimen)
+                    }
                 }
             }
-            
 
         } else {
             println("ERROR: Failed to locate user's Movies directory");
